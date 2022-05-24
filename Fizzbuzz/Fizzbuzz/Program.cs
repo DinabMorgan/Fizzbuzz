dde6f4ca8592d450a9b51808ac299610497c584f
DinabMorgan <morgan.dinab@gmail.com>
2022-05-24 16:36:31 -0400

    done code

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine(" Lets play FizzBuzz!");
+int count = 0;
+
+while (count <= 100) { 
+    
+
+       if (count % 15==0) 
+        
+    {
+        Console.WriteLine("FizzBuzz");
+
+    }
+    else if (count %5 == 0)
+    {
+        Console.WriteLine("BUZZ!!");
+    }
+    else if (count % 3 == 0)
+    { 
+    Console.WriteLine("FiZZ");
+    } 
+      else
+    {
+        Console.WriteLine(count);
+    }
+       count++;
+   }
